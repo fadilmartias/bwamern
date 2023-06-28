bca9d236eb2f402e33fe86af149dc3fa7f2fbf19
fadilmartias <fadilmartias26@gmail.com>
2023-06-28 13:09:49 +0700

    import images from figma, section hero 75% completed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from 'parts/Header'
+import Hero from 'parts/Hero'
+import landingPage from 'json/landingPage.json';
+
 function LandingPage() {
   return (
     <>
         <Header></Header>
+        <Hero data={landingPage.hero} />
     </>
   )
 }
